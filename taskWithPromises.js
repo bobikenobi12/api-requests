fb1c9900a70cdffaaae17653c5589c5752a19686
unknown <borislav1207@gmail.com>
2021-05-10 10:15:18 +0300

    remove comments

--- taskWithPromises.js
+++ taskWithPromises.js
@@ -104,41 +104,5 @@ fetch('https://randomuser.me/api/')
 
 
 
-// fetch('https://randomuser.me/api/')
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const firstName = data.results[0].name.first;
-//     const userGender = data.results[0].gender;
-//     const age = data.results[0].dob.age;
-//     console.log(firstName);
-//     console.log(age);
-//     fetch(`https://api.genderize.io/?name=${firstName}`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const gender = data.gender;
-//         const probability = data.probability;
-//         gender === userGender
-//           ? console.log('Gender verified')
-//           : console.log(
-//               `Gender discrepancy, probability of ${probability} is not enough.`
-//             );
-//         fetch('https://www.affirmations.dev/')
-//           .then((response) => response.json())
-//           .then((data) => {
-//             console.log(data.affirmation);
-//           })
-//           .catch((err) => console.log(`Error: ${err.message}`));
-//       })
-//       .catch((err) => console.log(`Error: ${err.message}`));
-//     fetch(`https://api.agify.io/?name=${firstName}`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // gender === userGender
-//         //   ? console.log('Gender verified')
-//         //   : console.log(
-//         //       `Gender discrepancy, probability of ${probability} is not enough.`
-//         //     );
-//       })
-//       .catch((err) => console.log(`Error: ${err.message}`));
-//   })
-//   .catch((err) => console.log(`Error: ${err.message}`));
+
+
