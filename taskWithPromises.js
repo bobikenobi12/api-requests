e739fbbb6e107cb438a8efcb7fa96b79bf38154e
unknown <borislav1207@gmail.com>
2021-05-16 13:01:06 +0300

    refactor code

--- taskWithPromises.js
+++ taskWithPromises.js
@@ -5,24 +5,23 @@ let userGender;
 let firstName;
 let userAge;
 let userCountry;
+
 fetch('https://randomuser.me/api/')
   .then(toJson)
   .then((data) => {
-    firstName = data.results[0].name.first;
+    const dataResults = data.results[0];
+    firstName = dataResults.name.first;
     for (let i = 0; i < firstName.length; i++) {
       const code = firstName.charCodeAt(i);
       if (!((code >= 97 && code <= 122) || (code >= 65 && code <= 90))) {
         throw new Error('The first name must contain only A-Z  letters!');
-      } else {
-        continue;
       }
-      
     }
-    userGender = data.results[0].gender;
-    userAge = data.results[0].dob.age;
-    userCountry = data.results[0].location.country;
-    userCountryCode = data.results[0].nat;
-    initialDate = new Date(data.results[0].dob.date);
+    userGender = dataResults.gender;
+    userAge = dataResults.dob.age;
+    userCountry = dataResults.location.country;
+    userCountryCode = dataResults.nat;
+    initialDate = new Date(dataResults.dob.date);
     userYearOfBirth = initialDate.getFullYear();
     console.log(`First name: ${firstName}`);
     console.log(`User gender: ${userGender}`);
@@ -33,10 +32,8 @@ fetch('https://randomuser.me/api/')
     return fetch(`https://api.genderize.io/?name=${firstName}`);
   })
   .then(toJson)
-  .then((data) => {
-    const predictedGender = data.gender;
-    const probability = data.probability;
-    predictedGender === userGender
+  .then(({ gender, probability }) => {
+    gender === userGender
       ? console.log('Gender verified.')
       : console.log(
           `Gender discrepancy, probability of ${probability} is not enough.`
@@ -44,50 +41,40 @@ fetch('https://randomuser.me/api/')
     return fetch('https://www.affirmations.dev/');
   })
   .then(toJson)
-  .then((data) => {
-    console.log(data.affirmation);
+  .then(({ affirmation }) => {
+    console.log(affirmation);
     return fetch(`https://api.agify.io/?name=${firstName}`);
   })
   .then(toJson)
-  .then((data) => {
-    const predictedAge = data.age;
-    console.log(`Predicted age: ${predictedAge}`);
-    predictedAge === userAge
+  .then(({ age }) => {
+    console.log(`Predicted age: ${age}`);
+    age === userAge
       ? console.log('Age verified.')
       : console.log(
           `Age discrepancy, there was ${Math.abs(
-            predictedAge - userAge
+            age - userAge
           )} years difference.`
         );
     return fetch(`https://www.affirmations.dev/`);
   })
   .then(toJson)
-  .then((data) => {
-    console.log(data.affirmation);
+  .then(({ affirmation }) => {
+    console.log(affirmation);
     return fetch(`https://api.nationalize.io/?name=${firstName}`);
   })
   .then(toJson)
-  .then((data) => {
-    let countryDataArray = data.country;
-    let countryIds = [];
-    let listOfOtherCountries = [];
-    let matchingValue = [];
-    for (let i = 0; i < countryDataArray.length; i++) {
-      countryIds.push(countryDataArray[i].country_id);
-      if (!countryIds[0]) {
-        throw new Error('No countries were matched to the specified user');
-      } else {
-        continue;
-      }
+  .then(({ country }) => {
+    const countryIds = country
+      .map((el) => el.country_id)
+      .filter((el) => el !== '');
+    if (countryIds.length === 0) {
+      console.log('No countries were matched to the specified user');
     }
-
-    for (let j = 0; j < countryIds.length; j++) {
-      if (countryIds[j] === userCountryCode) {
-        matchingValue.push(countryIds[j]);
-      } else {
-        listOfOtherCountries.push(countryIds[j]);
-      }
-    }
+    console.log(countryIds);
+    const matchingValue = countryIds.find((el) => el === userCountryCode);
+    const listOfOtherCountries = countryIds.filter(
+      (el) => el !== matchingValue
+    );
     if (matchingValue === userCountryCode) {
       console.log(
         `Country verified. Other possible countries: ${listOfOtherCountries.toString()} `
@@ -100,23 +87,17 @@ fetch('https://randomuser.me/api/')
     return fetch(`https://www.affirmations.dev/`);
   })
   .then(toJson)
-  .then((data) => {
-    console.log(data.affirmation);
+  .then(({ affirmation }) => {
+    console.log(affirmation);
     return fetch(
       `https://date.nager.at/Api/v2/PublicHolidays/${userYearOfBirth}/${userCountryCode.toLowerCase()}`
     );
   })
   .then(toJson)
   .then((data) => {
-    let filteredHolidays = data.filter(
-      (el) => el.launchYear > userYearOfBirth || el.launchYear === null
-    );
-    let formatArray = [];
-    for (let i = 0; i < filteredHolidays.length; i++) {
-      formatArray.push(
-        `${filteredHolidays[i].localName}(${filteredHolidays[i].name})`
-      );
-    }
-    console.log(formatArray.toString());
+    const filteredHolidays = data
+      .filter((el) => el.launchYear > userYearOfBirth || el.launchYear === null)
+      .map((el) => `${el.localName}(${el.name})`);
+    console.log(filteredHolidays);
   })
   .catch((err) => console.log(`Error: ${err.message}`));
